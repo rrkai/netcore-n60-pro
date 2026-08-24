aaef43752aff55417d6503def986322385a5821a
rrkai <59231078+rrkai@users.noreply.github.com>
2026-08-24 15:50:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,7 +15,7 @@ echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.defau
 # ===== 新增：PassWall 源 =====
 echo 'src-git passwall https://github.com/Openwrt-Passwall/openwrt-passwall.git;main' >>feeds.conf.default
 echo 'src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main' >>feeds.conf.default
-echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' >>feeds.conf.default
+# echo 'src-git passwall2 https://github.com/Openwrt-Passwall/openwrt-passwall2.git;main' >>feeds.conf.default
 
 # ===== 移除：AdGuardHome 源（原来有 git clone，现在删掉） =====
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
